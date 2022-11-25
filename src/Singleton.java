9d60a69135cee5eff6ed5b02f340b4b4fee1a860
revanhesenzada <hasanzadaravan@gmail.com>
2022-11-25 12:28:35 +0400

    SingletonDesignPattern

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Singleton {
+public class Singleton {
+    private static Singleton singleton;
+    private Singleton() {
+    }
+    public static Singleton getSingleton() {
+        if(singleton==null){
+            synchronized (Singleton.class){
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+
+        return singleton;
+    }
 }
